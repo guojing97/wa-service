f96e697aa8b6253fe17a6d3b35c9caf2786356a6
Kevin Hendrawan <projectkevin97@gmail.com>
2026-06-02 10:28:51 +0700

    refactor: remove redundant comments in rate limiter configurations

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -7,7 +7,7 @@ const ResponseHelper = require('../utils/responseHelper');
  */
 const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 1000, // Limit each IP to 1000 requests per windowMs
+  max: 1000, 
   standardHeaders: true,
   legacyHeaders: false,
   handler: (req, res) => {
@@ -15,12 +15,9 @@ const rateLimiter = rateLimit({
   },
 });
 
-/**
- * Stricter rate limiter for message sending
- */
 const messageLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 100, // 100 messages per minute
+  windowMs: 60 * 1000, 
+  max: 100, 
   standardHeaders: true,
   legacyHeaders: false,
   handler: (req, res) => {
